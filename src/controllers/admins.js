86ad821b03a2c3a3ebd746d8450fe42d7f9de4ba
jung0730 <arj2763@gmail.com>
2023-05-02 12:03:29 +0800

    feat/完成login

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -6,11 +6,13 @@ const errorHandler = require('service/errorHandler');
 const { isAuth, generateJWT } = require('service/auth');
 const admins = {
   async login(req, res, next) {
-    const { account } = req.body;
-    let { password } = req.body;
-    if (!account || !password) return next(errorHandler(400,"欄位未填寫正確"));
+    const account = req.body?.account;
+    let password = req.body?.password;
+    if (!account || !password) {
+      return next(errorHandler(400, '欄位未填寫正確'));
+    }
     if (!validator.isLength(password, { min: 8 })) {
-      return next(errorHandler(400,"密碼不得低於 8 字元"));
+      return next(errorHandler(400, '密碼不得低於 8 字元'));
     }
     password = await bcrypt.hash(password, 12);
     const data = await Admin.create({
