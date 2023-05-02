3e6bc7dab8ca1e5646bee5d3b1a540fcc2a65f44
jung0730 <arj2763@gmail.com>
2023-05-02 17:43:16 +0800

    feat: confirmPost 部分邏輯判斷

--- src/controllers/admins.js
+++ src/controllers/admins.js
@@ -9,6 +9,21 @@ const admins = {
     const posts = await Post.find({ status: 'pending' }).sort('createdAt');
     successHandler(res, posts);
   },
+  async confirmPost(req, res, next) {
+    const postId = req.body?.postId;
+    const status = req.body?.status;
+    const rejectReason = req.body?.rejectReason;
+    if (!postId || !status) {
+      return next(errorHandler(400, '欄位未填寫正確'));
+    }
+    if (status !== 'approved' || status !== 'rejected') {
+      return next(errorHandler(400, '審核狀態只能是「已通過」或「已拒絕」'));
+    }
+    if (status === 'rejected' && !rejectReason) {
+      return next(errorHandler(400, '未填寫拒絕原因'));
+    }
+    // TODO:
+  }
 };
 
 module.exports = admins;
--- src/routes/admins.js
+++ src/routes/admins.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const asyncErrorHandler = require('service/asyncErrorHandler');
-const { login, logout, getUnconfirmedPosts } = require('controllers/admins');
+const { login, logout, getUnconfirmedPosts, confirmPost } = require('controllers/admins');
 const { isAuth } = require('service/auth');
 router.post('/login', asyncErrorHandler(login));
 router.post('/logout', isAuth, asyncErrorHandler(logout));
-// TODO: 確認get or post
 router.get('/unconfirmedPosts', isAuth, asyncErrorHandler(getUnconfirmedPosts));
+router.post('/confirmPost', isAuth, asyncErrorHandler(confirmPost));
 module.exports = router;
